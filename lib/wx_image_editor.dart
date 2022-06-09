5c035bbf0eaa9ebb9924bbbf87895c6b139556d1
weichengwu <wuvcen@gmail.com>
2022-06-09 11:17:29 +0800

    判断是否编辑

--- lib/wx_image_editor.dart
+++ lib/wx_image_editor.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'wx_image_editor_platform_interface.dart';
 
 abstract class WxImageEditor {
-  static Future<void> editImage(String path) {
-    return WxImageEditorPlatform.instance.editImage(path);
+  static Future<bool> editImage(String path) async {
+    final time1 = await File(path).lastModified();
+    await WxImageEditorPlatform.instance.editImage(path);
+    final time2 = await File(path).lastModified();
+    return time1 != time2;
   }
 
   static Future<void> setup({
